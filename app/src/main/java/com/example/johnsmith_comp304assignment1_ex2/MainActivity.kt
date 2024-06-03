974694358dc4afac968b9e86f0f119c33788cf1c
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-03 07:31:01 -0400

    Update MainActivity Class

--- app/src/main/java/com/example/johnsmith_comp304assignment1_ex2/MainActivity.kt
+++ app/src/main/java/com/example/johnsmith_comp304assignment1_ex2/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.johnsmith_comp304assignment1_ex2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<View>(R.id.buttonFragment1).setOnClickListener {
+            loadFragment(FragmentOne())
         }
+
+        findViewById<View>(R.id.buttonFragment2).setOnClickListener {
+            loadFragment(FragmentTwo())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
